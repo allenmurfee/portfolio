19d72df175f437e8711087bf50925ebfa583988d
allenmurfee <allenmurfee123@gmail.com>
2024-07-17 16:21:28 -0400

    footer mobile icon centering

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const useFooter = () => {
     <Footer className="bg-zinc-700 rounded-none shadow-none mt-5" container>
       <div className="w-full sm:flex sm:items-center sm:justify-between bg-zinc-700">
         <Footer.Copyright href="#" by="Allen Murfee" year={2024} />
-        <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+        <div className="mt-4 flex space-x-6 sm:mt-0 justify-center">
           <Footer.Icon href="https://github.com/allenmurfee" icon={BsGithub} />
           <Footer.Icon
             href="https://www.linkedin.com/in/allen-murfee/"
